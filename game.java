e200e9232e18cae8ce6684c0f9b9daacc0a86b6e
templca <101844197+templca@users.noreply.github.com>
2022-05-13 22:15:21 +1200

    why is it like this
    really bad code! instead of conway gam of life it just inverts :          )

--- game.java
+++ game.java
@@ -4,6 +4,7 @@
  *
  * @author cara templeton
  * @version 12/05
+ * 13/05
  */
 public class game
 {
@@ -14,41 +15,38 @@ public class game
     int neighbours;
     int y;
     int x;
-    
 
     boolean check(boolean alive){
-        alive=present[x][y];
-        
-        if (present[x][y+1]==true) {
-            neighbours++;
-        } 
-        if (present[x][y-1]==true) {
-            neighbours++;
+        for (int ii=1;ii<20;ii++) {
+            alive=present[x][y];
+
+            if (present[x][y+1]==true) {
+                neighbours++;
+            } 
+            if (present[x+1][y]==true) {
+                neighbours++;
+            }
+            if (present[x+1][y+1]==true) {
+                neighbours++;
+            }
+
         }
-        if (present[x+1][y]==true) {
-            neighbours++;
-        }
-        if (present[x+1][y+1]==true) {
-            neighbours++;
-        }
-        if (present[x+1][y-1]==true) {
-            neighbours++;
-        }
-        if (present[x-1][y]==true) {
-            neighbours++;
-        }
-        if (present[x-1][y+1]==true) {
-            neighbours++;
-        }
-        if (present[x-1][y-1]==true) {
-            neighbours++;
-        }
-        
-        if (alive==true && neighbours==2||neighbours==3){ return alive;  }
-        else if (alive==false && neighbours==3) { return alive;}
-        else if (alive==true && neighbours < 2) { return!alive;}
-        else if (alive==true && neighbours > 3) { return !alive;}
-        else {return !alive;}
+        if (alive==true && neighbours==2||neighbours==3){ 
+            future[x][y]=true;
+            return alive;  }
+        else if (alive==false && neighbours==3) { 
+            future[x][y]=true;
+            return alive;}
+        else if (alive==true && neighbours < 2) { 
+            future[x][y]=false;
+            return!alive;}
+        else if (alive==true && neighbours > 3) { 
+            future[x][y]=false;
+            return !alive;}
+        else {
+            future[x][y]=false;
+            return !alive;}
+
     }
 
     /**
@@ -57,20 +55,30 @@ public class game
     public game()
     {
         // initialise instance variables
+        present[3][2]=true;
+        present[2][3]=true;
         present[2][2]=true;
         x = 0;
+        System.out.println("first gen");
         for (x=0;x<20;x++){
             for (y=0;y<20;y++){
-                if (check(present[x][y]==true)){
+                if (present[x][y]==true){
                     System.out.print("x ");
-                }
-                if (check(present[x][y]==false)){
-                    System.out.print("0 ");
-                }
+                } else System.out.print("0 ");
             }
             System.out.println();
         }
-        
+
+        System.out.println("next gen");
+        for (x=0;x<20;x++){
+            for (y=0;y<20;y++){
+                if (check(future[x][y]==true)){
+                    System.out.print("x ");
+                } else System.out.print("0 ");
+            }
+            System.out.println();
+        }
+
     }
 
     /**
